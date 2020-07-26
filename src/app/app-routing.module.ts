e4669fdbdcc6fecea5fe0f8cc30fb7adef94d00f
Omar <omar.colg97@gmail.com>
2020-07-26 01:41:35 -0600

    commit inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,55 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { PagesComponent } from './pages/pages.component';
+import { DashBoardComponent } from './pages/dash-board/dash-board.component';
+import { LoginComponent } from './login/login.component';
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { LandingComponent } from './landing/landing.component';
+import { RegistroComponent } from './registro/registro.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'index',
+    component: LandingComponent
+  },
+  {
+    path:'',
+    component: PagesComponent,
+    children:[
+      {
+        path:'dashboard', component: DashBoardComponent
+      },
+      {
+        path:'home', component: HomeComponent
+      },
+      {
+        path:'progress', component: ProgressComponent
+      },
+      {
+        path:'',redirectTo:'/',pathMatch:'full'
+      }
+    ]
+  },
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  {
+    path:'registro',
+    component: RegistroComponent
+  },
+  {
+    path:'**',
+    component: NoPageFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
-})
+}) 
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
+import { DashBoardComponent } from './pages/dash-board/dash-board.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SideBarComponent } from './shared/side-bar/side-bar.component';
+import { BreadCrumbsComponent } from './shared/bread-crumbs/bread-crumbs.component';
+import { PagesComponent } from './pages/pages.component';
+import { LandingComponent } from './landing/landing.component';
+import { RegistroComponent } from './registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NoPageFoundComponent,
+    DashBoardComponent,
+    HomeComponent,
+    ProgressComponent,
+    HeaderComponent,
+    SideBarComponent,
+    BreadCrumbsComponent,
+    PagesComponent,
+    LandingComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
