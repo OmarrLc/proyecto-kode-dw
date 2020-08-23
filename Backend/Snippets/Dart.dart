4dcb1f3f169f759b544f0c1dadb15187b489daca
Omar <omar.colg97@gmail.com>
2020-08-23 00:19:40 -0600

    Version final v1.1

---
+++ Backend/Snippets/Dart.dart
@@ -0,0 +1,20 @@
+import 'dart:html';
+
+/// Stores the image in [blob] in the [ImageElement] of the given [selector].
+void setImage(selector, blob) {
+  FileReader reader = new FileReader();
+  reader.onLoad.listen((fe) { 
+    ImageElement image = document.querySelector(selector);
+    image.src = reader.result;
+  });
+  reader.readAsDataUrl(blob);  
+}
+
+main() async {
+  var url = "https://upload.wikimedia.org/wikipedia/commons/2/28/Tortoiseshell_she-cat.JPG";
+
+  // Initiates a request and asynchronously waits for the result.
+  var request = await HttpRequest.request(url, responseType: 'blob');
+  var blob = request.response;
+  setImage("#cats", blob);
+}
